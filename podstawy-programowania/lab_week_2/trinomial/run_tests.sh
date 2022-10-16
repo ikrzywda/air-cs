0ed26aaae69917d36480b912c7c88acce8d9a5b8
ikrz <igor.krzywda@improvoc.com>
2022-10-16 15:02:08 +0200

    whoops

--- podstawy-programowania/lab_week_2/trinomial/run_tests.sh
+++ podstawy-programowania/lab_week_2/trinomial/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "COMPILING TESTS..."
-gcc -std=c99 -pedantic -Wall trinomial/tests.c -lm -o tests
+gcc -std=c99 -pedantic -Wall app/tests.c -lm -o tests
 
 echo "RUNNING TESTS..."
 ./tests
 
-echo "TESTS PASSED
+echo "TESTS PASSED"
 
 rm tests
