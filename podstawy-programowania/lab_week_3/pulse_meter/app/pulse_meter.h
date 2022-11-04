d41d2de5ca0e1f4d55bacf7cbf976efe53d8bcb5
ikrz <igor.krzywda@improvoc.com>
2022-11-04 23:41:24 +0100

    normalized and raw input toggled with flags

--- podstawy-programowania/lab_week_3/pulse_meter/app/pulse_meter.h
+++ podstawy-programowania/lab_week_3/pulse_meter/app/pulse_meter.h
@@ -16,4 +16,14 @@ float compute_frequency(float data_batch[BATCH_LENGTH], float duration) {
   return (change_count / duration) * (60 / duration);
 }
 
+float compute_frequency_normalized_input(int data_batch[BATCH_LENGTH]) {
+  int change_count = 0;
+  for (int i = 0; i < BATCH_LENGTH; ++i) {
+    if (!(data_batch[i - 1] < 0) != !(data_batch[i] < 0)) {
+      ++change_count;
+    }
+  }
+  return (change_count / NORMALIZED_INPUT_DURATION) * (60 / NORMALIZED_INPUT_DURATION);
+}
+
 #endif
