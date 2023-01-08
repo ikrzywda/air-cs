cc32bf7b6e46fe4ec4732211ea6304076e89a5c0
ikrz <igor.krzywda@improvoc.com>
2023-01-08 22:52:51 +0100

    tests outline

--- podstawy-programowania/laby/lab_week_4/pgm_transformer/app/tests/test_transform.c
+++ podstawy-programowania/laby/lab_week_4/pgm_transformer/app/tests/test_transform.c
@@ -1,17 +1,15 @@
-
 #include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-#include "pgm/menu.c"
-#include "pgm/reader.c"
-#include "pgm/transformer.c"
+#include "../pgm/reader.c"
+#include "../pgm/transformer.c"
+
+
 
 #define IMAGE_FIXTURE_WIDTH 5
 #define IMAGE_FIXTURE_HEIGHT 51
 #define IMAGE_FIXTURE_CONTENT_LENGTH 255
-#define BIT_DEPTH 255
+#define IMAGE_FIXTURE_BIT_DEPTH 255
 
 PGMImage *image_fixture(int range_start, int range_end) {
   PGMImage *image_fixture = malloc(sizeof(PGMImage));
@@ -20,25 +18,27 @@ PGMImage *image_fixture(int range_start, int range_end) {
   image_fixture->contents_length = IMAGE_FIXTURE_CONTENT_LENGTH;
   image_fixture->width = IMAGE_FIXTURE_WIDTH;
   image_fixture->height = IMAGE_FIXTURE_HEIGHT;
+  image_fixture->bit_depth = IMAGE_FIXTURE_BIT_DEPTH;
 
-  for (int i = range_start + 1; i <= range_end; ++i) {
-    image_fixture->contents = i;
+  for (int i = range_start; i <= range_end; ++i) {
+    image_fixture->contents[i] = i;
   }
 
   return image_fixture;
 }
 
 void test_invert() {
-  PGMImage *image = image_fixture(0, BIT_DEPTH);
+  PGMImage *image = image_fixture(0, IMAGE_FIXTURE_BIT_DEPTH);
 
   invert_image(image);
 
-  for (int i = 0; i < BIT_DEPTH; ++i) {
-    assert(i == BIT_DEPTH - i);
+  for (int i = 0; i < IMAGE_FIXTURE_CONTENT_LENGTH; ++i) {
+    assert(image->contents[i] == IMAGE_FIXTURE_BIT_DEPTH - i);
   }
+  printf("TEST INVERT PASSED\n");
 }
 
+
 int main(int argc, char **argv) {
-  repl_main_loop();
-  return 0;
+  test_invert();
 }
