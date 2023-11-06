09c29abcf08850bbfce60222c51c32a3c31cf666
ikrzywda <krzywdaigor890@gmail.com>
2023-11-06 14:12:14 +0100

    fizyka3.1/fixed-current-uncertainty

--- sem_3/fizyka-3.1/planck-constant/script.py
+++ sem_3/fizyka-3.1/planck-constant/script.py
@@ -29,19 +29,19 @@ def compute_current_uncertainty(
     value: float, *, dgt_uncertainty: float, unit_coeff: float
 ):
     nominal_value = value * unit_coeff
-    uncertainty = 0.008 if nominal_value < 0.02 else 0.012
+    uncertainty = 0.008 if nominal_value < 20 else 0.012
     return (
         compute_uncertainty(
             value,
             uncertainty=uncertainty,
             dgt_uncertainty=dgt_uncertainty,
-            dgt_count=3,
+            dgt_count=1,
         )
         * unit_coeff
     )
 
 
-def apply_voltage_and_current_uncertainties(df: pd.DataFrame) -> None:
+def apply_voltage_and_current_uncertainties(df: pd.DataFrame) -> pd.DataFrame:
     df["voltage_uncertainty"] = df.apply(
         lambda row: compute_voltage_uncertainty(
             row["voltage"],
@@ -58,9 +58,30 @@ def apply_voltage_and_current_uncertainties(df: pd.DataFrame) -> None:
         ),
         axis=1,
     )
+    df["voltage"] = df.apply(
+        lambda row: row["voltage"] * row["voltage_unit_coeff"], axis=1
+    )
+    df["current"] = df.apply(
+        lambda row: row["current"] * row["current_unit_coeff"], axis=1
+    )
+
+    df.drop(
+        columns=[
+            "voltage_unit_coeff",
+            "current_unit_coeff",
+            "voltage_dgt",
+            "current_dgt",
+        ],
+        inplace=True,
+    )
+
+    return df.reindex(
+        columns=["voltage", "voltage_uncertainty", "current", "current_uncertainty"]
+    )
 
 
-def apply_wavelength_uncertainties(df: pd.DataFrame) -> None:
+def apply_wavelength_uncertainties(df: pd.DataFrame) -> pd.DataFrame:
     df["wavelength_uncertainty"] = df.apply(
         lambda row: row["error"] / math.sqrt(3), axis=1
     )
+    return df
